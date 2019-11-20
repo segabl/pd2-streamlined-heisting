3e219a76f8e6752b99fe3a8e5d5248c550d70f65
Sebastian Gabl <sebastian.gabl@gmx.at>
2019-11-20 15:25:13 +0100

    Add asset group loading

--- lua/gamesetup.lua
+++ lua/gamesetup.lua
@@ -1,16 +1,18 @@
 
 Hooks:PostHook(GameSetup, "load_packages", "cass_load_packages", function (self)
 
-  if (PackageManager:loaded("packages/sm_wish")) then
-    CASS:log("Death sentence package is loaded")
+  local ai_group_type = tweak_data.levels:get_ai_group_type()
+  if ai_group_type == "murkywater" then
+    CASS:log("Murkywater faction package is loaded, loading custom Murky units...")
+    -- todo
+  elseif ai_group_type == "russian" then
+    CASS:log("Russian faction package is loaded, loading custom Russian units...")
+    -- todo
+  elseif PackageManager:loaded("packages/sm_wish") then
+    CASS:log("Death Sentence package is loaded, loading custom Zeal units...")
+    --CASS:load_asset_group("zeals")
   else
-    CASS:log("Death sentence package is not loaded")
-  end
-
-  if tweak_data.levels:get_ai_group_type() == "murkywater" then
-    CASS:log("Murkywater faction package is loaded")
-  else
-    CASS:log("Murkywater faction package is not loaded")
+    CASS:log("No special faction packages are loaded")
   end
 
 end)
--- mod.lua
+++ mod.lua
@@ -2,14 +2,30 @@ if not CASS then
 
   _G.CASS = {}
   CASS.mod_path = ModPath
+  CASS.logs = true
 
   function CASS:log(...)
+    if not self.logs then
+      return
+    end
     local params = {...}
     local str = ""
     for _, v in pairs(params) do
       str = str .. tostring(v) .. " "
     end
     log("[CASS] " .. str)
+  end
+
+  function CASS:load_asset_group(name)
+    if not self.asset_loader then
+      local mod = BLT.Mods:GetModOwnerOfFile(CASS.mod_path)
+      self.asset_loader = mod and mod.supermod and mod.supermod:GetAssetLoader()
+      if not self.asset_loader then
+        self:log("ERROR: Couldn't retrieve the mod's asset loader!")
+        return
+      end
+    end
+    self.asset_loader:LoadAssetGroup(name)
   end
 
 end
