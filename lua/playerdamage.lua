ee1494f2e6e8e297f53f48390e4f0c5232764758
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-02-11 23:53:01 +0100

    Add small damage threshold

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -4,8 +4,8 @@ function PlayerDamage:_chk_dmg_too_soon(damage)
   -- I-frames protect no matter the new potential damage but the remaining i-frame time
   -- will be reduced based on the new to previous damage ratio if the new damage is higher
   if t < next_allowed_dmg_t then
-    if damage > self._last_received_dmg then
-      self._next_allowed_dmg_t = Application:digest_value(t + (next_allowed_dmg_t - t) * (self._last_received_dmg / damage), true)
+    if damage > self._last_received_dmg + 0.1 then
+      self._next_allowed_dmg_t = Application:digest_value(t + (next_allowed_dmg_t - t) * (self._last_received_dmg / (damage - 0.1)), true)
       self._last_received_dmg = damage
     end
     return true
