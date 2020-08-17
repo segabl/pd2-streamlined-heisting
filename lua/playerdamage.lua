36100f1f7e7507581c61ed79ee7b3e5e5d6ec284
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-08-17 15:13:28 +0200

    Tweak preset values and grace period duration

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -1,13 +1,5 @@
+-- I-frames protect no matter the new potential damage but they are shorter in general
 function PlayerDamage:_chk_dmg_too_soon(damage)
-  local t = managers.player:player_timer():time()
   local next_allowed_dmg_t = type(self._next_allowed_dmg_t) == "number" and self._next_allowed_dmg_t or Application:digest_value(self._next_allowed_dmg_t, false)
-  -- I-frames protect no matter the new potential damage but the remaining i-frame time
-  -- will be reduced based on the new to previous damage ratio if the new damage is higher
-  if t < next_allowed_dmg_t then
-    if damage > self._last_received_dmg then
-      self._next_allowed_dmg_t = Application:digest_value(t + (next_allowed_dmg_t - t) * (self._last_received_dmg / damage), true)
-      self._last_received_dmg = damage
-    end
-    return true
-  end
+  return managers.player:player_timer():time() < next_allowed_dmg_t
 end
--- lua/playertweakdata.lua
+++ lua/playertweakdata.lua
@@ -1,21 +1,21 @@
 Hooks:PostHook(PlayerTweakData, "_set_easy", "sh__set_easy", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.425
+  self.damage.MIN_DAMAGE_INTERVAL = 0.55
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_normal", "sh__set_normal", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.4
+  self.damage.MIN_DAMAGE_INTERVAL = 0.5
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_hard", "sh__set_hard", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.375
+  self.damage.MIN_DAMAGE_INTERVAL = 0.45
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill", "sh__set_overkill", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.35
+  self.damage.MIN_DAMAGE_INTERVAL = 0.4
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill_145", "sh__set_overkill_145", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.325
+  self.damage.MIN_DAMAGE_INTERVAL = 0.35
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_easy_wish", "sh__set_easy_wish", function (self)
@@ -23,11 +23,11 @@ Hooks:PostHook(PlayerTweakData, "_set_easy_wish", "sh__set_easy_wish", function 
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_overkill_290", "sh__set_overkill_290", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.275
+  self.damage.MIN_DAMAGE_INTERVAL = 0.25
 end)
 
 Hooks:PostHook(PlayerTweakData, "_set_sm_wish", "sh__set_sm_wish", function (self)
-  self.damage.MIN_DAMAGE_INTERVAL = 0.25
+  self.damage.MIN_DAMAGE_INTERVAL = 0.2
 end)
 
 Hooks:PostHook(PlayerTweakData, "init", "sh_init", function (self)
