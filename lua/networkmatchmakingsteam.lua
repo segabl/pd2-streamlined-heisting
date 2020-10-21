f8f12841b1b301ac6d202adf68c446b71454e93a
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-10-21 13:09:05 +0200

    Append matchmaking key

--- lua/networkmatchmakingsteam.lua
+++ lua/networkmatchmakingsteam.lua
@@ -1,1 +1,2 @@
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "sh-1.1"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sh_v1.1"
+StreamHeist:log("Matchmaking key set to", NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY)
