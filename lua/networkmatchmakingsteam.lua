68ef283136ca30ed519a46f2def8983f875f5c20
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-12-14 11:31:12 +0100

    Automatically append mod version to matchmaking key

--- lua/networkmatchmakingsteam.lua
+++ lua/networkmatchmakingsteam.lua
@@ -1,2 +1,2 @@
-NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sh_v1.5"
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
 StreamHeist:log("Matchmaking key set to", NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY)
