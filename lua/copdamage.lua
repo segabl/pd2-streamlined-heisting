dae615fbde1a2584d8a2538353c08a92c31a854f
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-12-14 10:32:13 +0100

    Adjust damage network messages for health granularity
    The new health granularity requires network messages that send damage to accept higher max values.
    Health granularity in vanilla is 512 steps, while the new granularity in this mod is 8192.
    While this granularity was already in effect, the network messages have not been adjusted until now which would lead to damage desync between clients and the host.

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,2 +1,4 @@
--- Health granularity fucks with linear damage interpolation of AI against other AI, so we increase it to mitigate some of the issues
+-- Health granularity prevents linear damage interpolation of AI against other AI from working
+-- correctly and notably rounds up damage against enemies with a high HP pool even for player weapons.
+-- Increasing the health granularity makes damage dealt more accurate to the actual weapon damage stats
 CopDamage._HEALTH_GRANULARITY = 8192
