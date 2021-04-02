fe3f4bf1381d1ce04d2c8b4d4695d242e77d273d
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-04-02 20:50:49 +0200

    Add delay to SWAT turret repair
    Gives a short time window to still deal damage after the shield breaks

---
+++ lua/sentrygundamage.lua
@@ -0,0 +1,14 @@
+-- Add a small delay before SWAT turrets retract for repair when their shield breaks
+Hooks:PostHook(SentryGunDamage, "_apply_damage", "sh__apply_damage", function (self, damage, dmg_shield)
+	if dmg_shield and self._shield_health <= 0 and not self._shield_repair_t then
+		self._shield_repair_t = TimerManager:game():time() + 2
+	end
+end)
+
+Hooks:PostHook(SentryGunDamage, "repair_shield", "sh_repair_shield", function (self)
+	self._shield_repair_t = nil
+end)
+
+function SentryGunDamage:needs_repair()
+	return self._shield_health <= 0 and self._shield_repair_t and TimerManager:game():time() > self._shield_repair_t
+end
