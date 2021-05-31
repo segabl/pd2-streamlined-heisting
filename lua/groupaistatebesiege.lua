b99afa463a0e46a9613354675c76c4c7a71d3f22
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-05-31 10:57:25 +0200

    Reduce amount of hostages needed to get max assault delay

--- lua/groupaistatebesiege.lua
+++ lua/groupaistatebesiege.lua
@@ -9,7 +9,7 @@ function GroupAIStateBesiege:_begin_assault_task(...)
 		local assault_task = self._task_data.assault
 		local anticipation_duration = self:_get_anticipation_duration(self._tweak_data.assault.anticipation_duration, assault_task.was_first)
 		local hesitation_delay = self:_get_difficulty_dependent_value(self._tweak_data.assault.hostage_hesitation_delay)
-		local hostage_multiplier = math.clamp(self._hostage_headcount / 2, 1, 4)
+		local hostage_multiplier = math.clamp(self._hostage_headcount, 1, 4)
 		assault_task.phase_end_t = self._t + anticipation_duration + hesitation_delay * hostage_multiplier
 		assault_task.is_hesitating = true
 		assault_task.voice_delay = self._t + (assault_task.phase_end_t - self._t) / 2
