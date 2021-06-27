837ccbbab8f8f456288794e777e69d45ead9bea5
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-06-27 20:14:03 +0200

    Fix Crime Spree heavies modifier
    Now properly replaces light rifle units with heavy rifle units and light shotgunners with heavy shotgunners instead of the other way around

---
+++ lua/modifierheavies.lua
@@ -0,0 +1,3 @@
+-- Fix incorrect heavy modifier (originally replaced rifle lights with shotgun heavies)
+ModifierHeavies.unit_swaps["units/payday2/characters/ene_fbi_swat_1/ene_fbi_swat_1"] = "units/payday2/characters/ene_fbi_heavy_1/ene_fbi_heavy_1"
+ModifierHeavies.unit_swaps["units/payday2/characters/ene_fbi_swat_2/ene_fbi_swat_2"] = "units/payday2/characters/ene_fbi_heavy_r870/ene_fbi_heavy_r870"
