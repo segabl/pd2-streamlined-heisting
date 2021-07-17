7d8b98db3ce81d8c5d694712efe2415aa9b2209d
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-07-17 12:33:42 +0200

    Stop dead enemies playing kill taunt voicelines

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -13,3 +13,13 @@ end
 Hooks:PostHook(PlayerDamage, "_send_damage_drama", "sh__send_damage_drama", function (self, attack_data, health_subtracted)
 	self._last_dodged = health_subtracted == 0
 end)
+
+
+-- Stop dead enemies from making kill taunts
+function PlayerDamage:clbk_kill_taunt(attack_data)
+	local attacker_unit = attack_data.attacker_unit
+	if alive(attacker_unit) and not attacker_unit:character_damage():dead() then
+		attacker_unit:sound():say("post_kill_taunt")
+	end
+	self._kill_taunt_clbk_id = nil
+end
