69e1f90def5400350ef45d6b69817b827c7231fe
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-07-20 14:47:31 +0200

    Simplify code

--- lua/copbrain.lua
+++ lua/copbrain.lua
@@ -1,15 +1,12 @@
 -- Always make enemies with special attack logics important
 Hooks:PostHook(CopBrain, "post_init", "sh_post_init", function (self)
 	if self._logics.attack ~= CopLogicAttack then
-		self:set_important(true)
 		self._forced_important = true
+		self:set_important(true)
 	end
 end)
 
 local set_important_original = CopBrain.set_important
 function CopBrain:set_important(state, ...)
-	if self._forced_important then
-		return
-	end
-	return set_important_original(self, state, ...)
+	return set_important_original(self, self._forced_important and true or state, ...)
 end
