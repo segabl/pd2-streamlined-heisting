422720981e3e114e89ad9af6d323e27d97dc48e6
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-07-28 14:23:26 +0200

    Simplify asset loading
    Check for loaded units instead of packages and avoid loading units that are already loaded

--- lua/dynamicresourcemanager.lua
+++ lua/dynamicresourcemanager.lua
@@ -1,21 +1,24 @@
 local ids_unit = Idstring("unit")
 Hooks:PostHook(DynamicResourceManager, "preload_units", "sh_preload_units", function (self)
 	local function load_unit(path)
-		self:load(ids_unit, Idstring(path), self.DYN_RESOURCES_PACKAGE)
-		self:load(ids_unit, Idstring(path .. "_husk"), self.DYN_RESOURCES_PACKAGE)
+		if not self:has_resource(ids_unit, Idstring(path), self.DYN_RESOURCES_PACKAGE) then
+			self:load(ids_unit, Idstring(path), self.DYN_RESOURCES_PACKAGE)
+			self:load(ids_unit, Idstring(path .. "_husk"), self.DYN_RESOURCES_PACKAGE)
+		end
 	end
 
-	StreamHeist:log("Loading custom units...")
-	load_unit("units/payday2/characters/ene_sniper_3/ene_sniper_3")
+	if PackageManager:has(ids_unit, Idstring("units/payday2/characters/ene_sniper_1/ene_sniper_1")) then
+		StreamHeist:log("Loading custom SWAT units...")
+		load_unit("units/payday2/characters/ene_sniper_3/ene_sniper_3")
+	end
 
-	-- Check for ai group type and also "packages/narr_jerry1" to support Proving Grounds
-	if tweak_data.levels:get_ai_group_type() == LevelsTweakData.LevelType.Murkywater or PackageManager:loaded("packages/narr_jerry1") then
-		StreamHeist:log("Murky package loaded, loading custom Murky units...")
+	if PackageManager:has(ids_unit, Idstring("units/pd2_dlc_bph/characters/ene_murkywater_shield/ene_murkywater_shield")) then
+		StreamHeist:log("Loading custom Murky units...")
 		load_unit("units/pd2_dlc_bph/characters/ene_murkywater_shield_c45/ene_murkywater_shield_c45")
 	end
 
-	if PackageManager:loaded("packages/sm_wish") then
-		StreamHeist:log("Zeal package loaded, loading custom Zeal units...")
+	if PackageManager:has(ids_unit, Idstring("units/pd2_dlc_gitgud/characters/ene_zeal_swat/ene_zeal_swat")) then
+		StreamHeist:log("Loading custom Zeal units...")
 		load_unit("units/pd2_dlc_gitgud/characters/ene_zeal_swat_2/ene_zeal_swat_2")
 		load_unit("units/pd2_dlc_gitgud/characters/ene_zeal_swat_heavy_2/ene_zeal_swat_heavy_2")
 		load_unit("units/pd2_dlc_gitgud/characters/ene_zeal_medic_m4/ene_zeal_medic_m4")
