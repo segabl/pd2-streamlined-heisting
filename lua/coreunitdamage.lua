6e8a40d9d192c62ad8ad27fa3b1b29a2c61adc47
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-08-26 21:39:30 +0200

    Improve Bulldozer check

--- lua/coreunitdamage.lua
+++ lua/coreunitdamage.lua
@@ -1,8 +1,7 @@
 -- Make bulldozer armor scale with difficulty
 local hp_muls = { 1, 1, 1.5, 2, 3, 4, 6, 8 }
 Hooks:PostHook(CoreBodyDamage, "init", "sh_init", function (self)
-	local unit_type = self._body_element and self._unit:base() and self._unit:base()._tweak_table
-	if unit_type and unit_type:find("^tank") then
+	if self._body_element and self._unit:base() and self._unit:base().has_tag and self._unit:base():has_tag("tank") then
 		if not CoreBodyDamage._tank_armor_multiplier then
 			local diff_i = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 			CoreBodyDamage._tank_armor_multiplier = 1 / hp_muls[diff_i]
