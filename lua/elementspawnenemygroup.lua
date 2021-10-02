e3988c211b233330fe7d42c4a1a24a7f0a869e59
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-10-02 19:36:53 +0200

    Add short delays to spawn groups with special spawn actions

--- lua/elementspawnenemygroup.lua
+++ lua/elementspawnenemygroup.lua
@@ -1,4 +1,4 @@
--- Update preferred spawn groups to contain all used groups
+-- Update preferred spawn groups to contain all used groups and add intervals to groups with special spawn actions
 local all_groups = {}
 local group_ai_tweak = tweak_data.group_ai
 for name, _ in pairs(group_ai_tweak.enemy_spawn_groups) do
@@ -8,10 +8,19 @@ for name, _ in pairs(group_ai_tweak.enemy_spawn_groups) do
 	end
 end
 
-Hooks:PostHook(ElementSpawnEnemyGroup, "_finalize_values", "sh__finalize_values", function(self)
-	if not self._values.preferred_spawn_groups or #self._values.preferred_spawn_groups <= 1 then
-		return
+Hooks:PostHook(ElementSpawnEnemyGroup, "_finalize_values", "sh__finalize_values", function (self)
+	if self._values.interval == 0 then
+		for _, id in pairs(self._values.elements) do
+			local spawn_point = self:get_mission_element(id)
+			if spawn_point and spawn_point._values.spawn_action then
+				self._values.interval = 5
+				StreamHeist:log(self:editor_name(), "0s -> 5s interval, spawn action", tostring(CopActionAct._act_redirects.enemy_spawn[spawn_point._values.spawn_action]))
+				break
+			end
+		end
 	end
-	StreamHeist:log(self:editor_name(), "preferred_spawn_groups", #self._values.preferred_spawn_groups, "->", #all_groups, "entries")
-	self._values.preferred_spawn_groups = all_groups
+	if self._values.preferred_spawn_groups and #self._values.preferred_spawn_groups > 1 then
+		StreamHeist:log(self:editor_name(), "preferred_spawn_groups", #self._values.preferred_spawn_groups, "->", #all_groups, "entries")
+		self._values.preferred_spawn_groups = all_groups
+	end
 end)
