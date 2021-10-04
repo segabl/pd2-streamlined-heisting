bea33096a3e987d9d4fc455d99a247356c8eb491
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-10-04 10:35:12 +0200

    Slightly reduce Bulldozer glass visor health
    Visor plate health is unchanged, glass visor health uses half the multiplier of the other armor parts now

--- lua/coreunitdamage.lua
+++ lua/coreunitdamage.lua
@@ -5,7 +5,7 @@ Hooks:PostHook(CoreBodyDamage, "init", "sh_init", function (self)
 		if not CoreBodyDamage._tank_armor_multiplier then
 			local diff_i = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 			CoreBodyDamage._tank_armor_multiplier = 1 / hp_muls[diff_i]
-			CoreBodyDamage._tank_glass_multiplier = 1 / math.max(1, hp_muls[diff_i] * 0.75)
+			CoreBodyDamage._tank_glass_multiplier = 1 / math.max(1, hp_muls[diff_i] * 0.5)
 		end
 		self._body_element._damage_multiplier = self._body_element._name:find("glass") and CoreBodyDamage._tank_glass_multiplier or CoreBodyDamage._tank_armor_multiplier
 	end
