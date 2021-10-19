4247faea05e64c83b33ea41b56d71d1ce8f9fb0a
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-10-19 19:12:52 +0200

    Tweak amount of enemies for Holdout
    Since this mode usually has very small playable areas and starts at full in-heist difficulty, the amount of enemies paired with all the fixes in this mod made Holdout very overwhelming

---
+++ lua/skirmishtweakdata.lua
@@ -0,0 +1,6 @@
+-- Reduce the amount of enemies in Holdout as the mission area is small and it is wave based
+Hooks:PostHook(SkirmishTweakData, "_init_group_ai_data", "sh__init_group_ai_data", function (self, tweak_data)
+	local skirmish_data = tweak_data.group_ai.skirmish
+	skirmish_data.assault.force = { 5, 5, 5 }
+	skirmish_data.assault.force_pool = { 200, 200, 200 }
+end)
