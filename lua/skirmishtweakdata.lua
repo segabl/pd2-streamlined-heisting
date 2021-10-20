fbdd80f9baa02547f2996baf30daac04641651f8
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-10-20 20:02:37 +0200

    Reduce health scaling on Holdout
    To compensate for the lower headshot multiplier on Overkill compared to vanilla

--- lua/skirmishtweakdata.lua
+++ lua/skirmishtweakdata.lua
@@ -4,3 +4,16 @@ Hooks:PostHook(SkirmishTweakData, "_init_group_ai_data", "sh__init_group_ai_data
 	skirmish_data.assault.force = { 7, 7, 7 }
 	skirmish_data.assault.force_pool = { 200, 200, 200 }
 end)
+
+
+-- Start damage/health scaling at 1 and scale health lower (nobody likes bullet sponges)
+Hooks:PostHook(SkirmishTweakData, "_init_wave_modifiers", "sh__init_wave_modifiers", function (self)
+	local wave_data = {}
+	for i = 0, 20 do
+		table.insert(wave_data, {
+			damage = 1 + i * 0.5,
+			health = 1 + i * 0.5
+		})
+	end
+	self.wave_modifiers[1][1].data.waves = wave_data
+end)
