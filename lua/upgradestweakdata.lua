4b7efe7839c8cc07339a4c60d9358b8b332c7955
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-11-09 18:11:37 +0100

    Remove changes to joker health multiplier
    Handled in Streamlined Heisting Complements

--- lua/upgradestweakdata.lua
+++
@@ -1,5 +0,0 @@
--- Remove the hidden 0.45 damage reduction multiplier stacked on top of the skill bonuses
--- of the partners in crime skill to balance out the increase in joker accuracy
-Hooks:PostHook(UpgradesTweakData, "_init_pd2_values", "sh__init_pd2_values", function (self)
-	self.values.player.convert_enemies_health_multiplier = { 1 }
-end)
