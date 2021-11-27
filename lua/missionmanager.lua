c401a07e7e71f73a868a3dce2111e4dddd52e34a
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-11-27 21:41:37 +0100

    Add reinforce points to Dragon Heist

--- lua/missionmanager.lua
+++ lua/missionmanager.lua
@@ -1,10 +1,27 @@
--- Add custom reinforce locations to specific levels
+-- Add custom reinforce locations to specific levels (trigger that has just a name disables previously enabled reinforcement with that id)
 local level_reinforce_triggers = {
 	des = {
-		[100304] = { name = "main_hall", force = 9, position = Vector3(-120, -2400, 100), disable_id = 100286 } -- Henry's Rock main hall
+		[100304] = {
+			{ name = "main_hall", force = 9, position = Vector3(-120, -2400, 100) }
+		},
+		[100286] = {
+			{ name = "main_hall" }
+		}
 	},
 	sah = {
-		[101400] = { name = "main_room", force = 5, position = Vector3(0, 2000, -100) } -- Shacklethorn auction room
+		[101400] = {
+			{ name = "auction_room", force = 5, position = Vector3(0, 2000, -100) }
+		}
+	},
+	chas = {
+		[101190] = {
+			{ name = "store_front1", force = 3, position = Vector3(-2000, 300, -10) },
+			{ name = "store_front2", force = 3, position = Vector3(-1000, 300, -10) }
+		},
+		[101647] = {
+			{ name = "store_front2" },
+			{ name = "back_alley", force = 3, position = Vector3(-1400, 4900, 540) }
+		}
 	}
 }
 local reinforce_triggers = level_reinforce_triggers[Global.game_settings and Global.game_settings.level_id]
@@ -12,17 +29,16 @@ if not reinforce_triggers then
 	return
 end
 
-local function hook_element(mission_manager, element_id, name, force, position, disable_id)
+local function hook_element(mission_manager, element_id, data)
 	local element = mission_manager:get_element_by_id(element_id)
 	if element then
 		Hooks:PostHook(element, "on_executed", "sh_on_executed_" .. element_id, function ()
-			StreamHeist:log("Mission script element", element_id, "executed,", force and "enabled" or "disabled", name, "reinforce point")
-			managers.groupai:state():set_area_min_police_force(name, force, position)
+			StreamHeist:log("Mission script element", string.format("%u (%s)", element_id, element:editor_name()), "executed, toggled", #data, "reinforce point(s)")
+			for _, v in pairs(data) do
+				managers.groupai:state():set_area_min_police_force(v.name, v.force, v.position)
+			end
 		end)
-		StreamHeist:log("Mission script element", element_id, "hooked as", name, "reinforce", force and "activation" or "deactivation", "trigger")
-		if disable_id then
-			hook_element(mission_manager, disable_id, name)
-		end
+		StreamHeist:log("Mission script element", string.format("%u (%s)", element_id, element:editor_name()), "hooked as reinforce trigger for", #data, "area(s)")
 	else
 		StreamHeist:log("[Error] Mission script element", element_id, "could not be found")
 	end
@@ -30,6 +46,6 @@ end
 
 Hooks:PostHook(MissionManager, "_activate_mission", "sh__activate_mission", function (self)
 	for element_id, data in pairs(reinforce_triggers) do
-		hook_element(self, element_id, data.name, data.force, data.position, data.disable_id)
+		hook_element(self, element_id, data)
 	end
 end)
