93266974b4b01505ea50224b290051becfadc07f
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-12-22 00:29:50 +0100

    Fix attention synchronization
    Will properly unregister attention object on clients now, specifically when players are targeted by AI

--- lua/copmovement.lua
+++ lua/copmovement.lua
@@ -6,3 +6,29 @@ function CopMovement:action_request(action_desc, ...)
 	end
 	return action_request_original(self, action_desc, ...)
 end
+
+
+-- Fix attention synch not providing the old attention
+function CopMovement:synch_attention(attention)
+	if attention and self._unit:character_damage():dead() then
+		StreamHeist:log("[Warning] Attention synch to dead unit")
+	end
+
+	self:_remove_attention_destroy_listener(self._attention)
+	self:_add_attention_destroy_listener(attention)
+
+	if attention and attention.unit and not attention.destroy_listener_key then
+		StreamHeist:log("[Warning] Attention synch with invalid data")
+		return self:synch_attention(nil)
+	end
+
+	local old_attention = self._attention
+	self._attention = attention
+	self._action_common_data.attention = attention
+
+	for _, action in ipairs(self._active_actions) do
+		if action and action.on_attention then
+			action:on_attention(attention, old_attention)
+		end
+	end
+end
