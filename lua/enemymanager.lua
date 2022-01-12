32dd0ce9136ecf04b14bff06df9aea3b9f0e26f9
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-01-12 14:18:35 +0100

    Optimize EnemyManager.get_nearby_medic

--- lua/enemymanager.lua
+++ lua/enemymanager.lua
@@ -1,14 +1,21 @@
--- Make medics require line of sight to heal
+-- Make medics require line of sight to heal and optimize function
 function EnemyManager:get_nearby_medic(unit)
 	if self:is_civilian(unit) then
 		return
 	end
 
+	local gstate = managers.groupai:state()
+	local medics = gstate._special_units.medic
+	if not medics then
+		return
+	end
+
+	local radius_sq = tweak_data.medic.radius ^ 2
 	local unit_pos = unit:movement():m_head_pos()
-	local enemies = World:find_units_quick(unit, "sphere", unit_pos, tweak_data.medic.radius, managers.slot:get_mask("enemies"))
-	for _, enemy in pairs(enemies) do
-		if enemy:base():has_tag("medic") and not World:raycast("ray", unit_pos, enemy:movement():m_head_pos(), "slot_mask", managers.slot:get_mask("AI_visibility")) then
-			return enemy
+	for u_key, _ in pairs(medics) do
+		local medic = gstate._police[u_key]
+		if mvector3.distance_sq(medic.m_pos, unit_pos) <= radius_sq and not World:raycast("ray", unit_pos, medic.unit:movement():m_head_pos(), "slot_mask", managers.slot:get_mask("AI_visibility")) then
+			return medic.unit
 		end
 	end
 end
