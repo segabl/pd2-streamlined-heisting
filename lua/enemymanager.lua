38c4030ee6be6ccdce851a20a5610771237a0f26
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-01-18 14:50:53 +0100

    Check medic in enemymanager unit data

--- lua/enemymanager.lua
+++ lua/enemymanager.lua
@@ -12,8 +12,9 @@ function EnemyManager:get_nearby_medic(unit)
 
 	local radius_sq = tweak_data.medic.radius ^ 2
 	local unit_pos = unit:movement():m_head_pos()
+	local unit_data = self._enemy_data.unit_data
 	for u_key, _ in pairs(medics) do
-		local medic = gstate._police[u_key]
+		local medic = unit_data[u_key]
 		if medic and unit ~= medic.unit and mvector3.distance_sq(medic.m_pos, unit_pos) <= radius_sq and not World:raycast("ray", unit_pos, medic.unit:movement():m_head_pos(), "slot_mask", managers.slot:get_mask("AI_visibility")) then
 			return medic.unit
 		end
