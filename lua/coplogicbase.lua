f2cad70a129d853dcdc30252a39cd61a33c7329b
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-03-08 17:07:37 +0100

    Stop following dead units
    If a unit that is a follow_unit of another unit dies, the other unit will immediately stop following it instead of waiting for the corpse to despawn

--- lua/coplogicbase.lua
+++ lua/coplogicbase.lua
@@ -48,6 +48,14 @@ Hooks:PreHook(CopLogicBase, "on_new_objective", "sh_on_new_objective", function 
 end)
 
 
+-- Remove follow unit as soon as it dies, not just after the body despawned
+function CopLogicBase.on_objective_unit_damaged(data, unit, attacker_unit)
+	if unit:character_damage()._dead then
+		data.objective_failed_clbk(data.unit, data.objective)
+	end
+end
+
+
 -- Allow more dodge directions
 local math_random = math.random
 local mvec3_add = mvector3.add
