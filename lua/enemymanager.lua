38eea678e158af32cd1d0560c25aa23e9769a57a
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-04-03 19:55:33 +0200

    Use report raycast

--- lua/enemymanager.lua
+++ lua/enemymanager.lua
@@ -15,7 +15,7 @@ Hooks:OverrideFunction(EnemyManager, "get_nearby_medic", function (self, unit)
 	local unit_data = self._enemy_data.unit_data
 	for u_key, _ in pairs(medics) do
 		local medic = unit_data[u_key]
-		if medic and unit ~= medic.unit and mvector3.distance_sq(medic.m_pos, unit_pos) <= radius_sq and not World:raycast("ray", unit_pos, medic.unit:movement():m_head_pos(), "slot_mask", managers.slot:get_mask("AI_visibility")) then
+		if medic and unit ~= medic.unit and mvector3.distance_sq(medic.m_pos, unit_pos) <= radius_sq and not World:raycast("ray", unit_pos, medic.unit:movement():m_head_pos(), "slot_mask", managers.slot:get_mask("AI_visibility"), "report") then
 			return medic.unit
 		end
 	end
