5f4eb57d73114cd100b3b116a300ee8761de8ffb
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-04-21 11:50:55 +0200

    Move NPC throwable loading to CopBase
    To guarantee clients also load throwable units in case they need dynamic loading

--- lua/copbase.lua
+++ lua/copbase.lua
@@ -1,15 +1,22 @@
--- Load npc throwables dynamically
+-- Dynamically load throwable if we have one
 local unit_ids = Idstring("unit")
-Hooks:PreHook(ProjectileBase, "throw_projectile_npc", "sh_throw_projectile_npc", function (projectile_type)
-	local tweak_entry = tweak_data.blackmarket.projectiles[projectile_type]
+Hooks:PostHook(CopBase, "init", "sh_init", function (self)
+	local throwable = self._char_tweak.throwable
+	if not throwable then
+		return
+	end
+
+	local tweak_entry = tweak_data.blackmarket.projectiles[throwable]
 	local unit_name = Idstring(not Network:is_server() and tweak_entry.local_unit or tweak_entry.unit)
+	local sprint_unit_name = tweak_entry.sprint_unit and Idstring(tweak_entry.sprint_unit)
+
 	if not PackageManager:has(unit_ids, unit_name) then
-		StreamHeist:log("Loading projectile unit", projectile_type)
+		StreamHeist:log("Loading projectile unit", throwable)
 		managers.dyn_resource:load(unit_ids, unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE)
 	end
-	local sprint_unit_name = tweak_entry.sprint_unit and Idstring(tweak_entry.sprint_unit)
+
 	if sprint_unit_name and not PackageManager:has(unit_ids, sprint_unit_name) then
-		StreamHeist:log("Loading projectile sprint unit", projectile_type)
+		StreamHeist:log("Loading projectile sprint unit", throwable)
 		managers.dyn_resource:load(unit_ids, sprint_unit_name, managers.dyn_resource.DYN_RESOURCES_PACKAGE)
 	end
 end)
