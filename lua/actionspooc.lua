segabl/pd2-streamlined-heisting

commit b09a0bf2b5f1e3a98d9561bd200d804d9c4b2ef2

Fix Bulldozer armor multiplier not being applied
As client or when the Bulldozer was a Medicdozer, the armor multiplier and armor blocking bullets were not applied correctly

supermod.xml
@@ -59,10 +59,17 @@
 				<post :hook_id="copmovement"/>
 			</group>
 			<post :hook_id="enemies/shield/logics/shieldlogicattack"/>
-			<post :hook_id="enemies/spooc/actions/lower_body/actionspooc"/>
-			<post :hook_id="enemies/spooc/logics/spooclogicattack"/>
-			<post :hook_id="enemies/tank/logics/tankcoplogicattack"/>
-			<post :hook_id="enemies/tank/tankcopdamage"/>
+			<group :hook_id="enemies/spooc/">
+				<post :hook_id="enemies/spooc/actions/lower_body/actionspooc"/>
+				<post :hook_id="enemies/spooc/logics/spooclogicattack"/>
+			</group>
+			<group :hook_id="enemies/tank/">
+				<post :hook_id="logics/tankcoplogicattack"/>
+				<post :hook_id="husktankcopdamage"/>
+				<post :hook_id="husktankmediccopdamage"/>
+				<post :hook_id="tankcopdamage"/>
+				<post :hook_id="tankmediccopdamage"/>
+			</group>
 			<post :hook_id="enemies/taser/logics/taserlogicattack"/>
 			<post :hook_id="environment/environmentfire"/>
 			<post :hook_id="equipment/sentry_gun/sentrygunbase"/>

lua/coreunitdamage.lua
@@ -1,7 +1,7 @@
 -- Make bulldozer armor scale with difficulty
 local hp_muls = { 1, 1, 1.5, 2, 3, 4, 6, 8 }
 Hooks:PostHook(CoreBodyDamage, "init", "sh_init", function (self)
-	if self._body_element and getmetatable(self._unit:character_damage()) == TankCopDamage then
+	if self._body_element and self._unit:character_damage() and self._unit:character_damage().IS_TANK then
 		if not CoreBodyDamage._tank_armor_multiplier then
 			local diff_i = tweak_data:difficulty_to_index(Global.game_settings and Global.game_settings.difficulty or "normal")
 			CoreBodyDamage._tank_armor_multiplier = 1 / hp_muls[diff_i]

lua/husktankcopdamage.lua
@@ -1,0 +1,4 @@
+-- Make Bulldozer armor prevent damage
+HuskTankCopDamage.IS_TANK = true
+
+HuskTankCopDamage.damage_bullet = TankCopDamage.damage_bullet

lua/husktankmediccopdamage.lua
@@ -1,0 +1,4 @@
+-- Make Bulldozer armor prevent damage
+HuskTankMedicCopDamage.IS_TANK = true
+
+HuskTankMedicCopDamage.damage_bullet = TankCopDamage.damage_bullet

lua/tankcopdamage.lua
@@ -1,4 +1,6 @@
 -- Make Bulldozer armor prevent damage
+TankCopDamage.IS_TANK = true
+
 local armor_body_names = {
 	[Idstring("body_armor_back"):key()] = true,
 	[Idstring("body_armor_chest"):key()] = true,

lua/tankmediccopdamage.lua
@@ -1,0 +1,4 @@
+-- Make Bulldozer armor prevent damage
+TankMedicCopDamage.IS_TANK = true
+
+TankMedicCopDamage.damage_bullet = TankCopDamage.damage_bullet

commit a34a9db392eba48964f51df6e37b4deae552f87c

Fix broken Cloaker hooks caused by b09a0bf2b5f1e3a98d9561bd200d804d9c4b2ef2

lua/actionspooc.lua
@@ -1,4 +1,4 @@
 -- Fix endless Cloaker beatdown
 function ActionSpooc:complete()
-	return self._beating_end_t and self._beating_end_t < TimerManager:game():time() and (not self:is_flying_strike() or self._last_vel_z >= 0)
+	return self._beating_end_t and self._beating_end_t < TimerManager:game():time() and (not self:is_flying_strike() or self._last_vel_z == 0)
 end

supermod.xml
@@ -62,8 +62,8 @@
 			</group>
 			<post :hook_id="enemies/shield/logics/shieldlogicattack"/>
 			<group :hook_id="enemies/spooc/">
-				<post :hook_id="enemies/spooc/actions/lower_body/actionspooc"/>
-				<post :hook_id="enemies/spooc/logics/spooclogicattack"/>
+				<post :hook_id="actions/lower_body/actionspooc"/>
+				<post :hook_id="logics/spooclogicattack"/>
 			</group>
 			<group :hook_id="enemies/tank/">
 				<post :hook_id="logics/tankcoplogicattack"/>
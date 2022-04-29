a34a9db392eba48964f51df6e37b4deae552f87c
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-04-29 16:50:06 +0200

    Fix broken Cloaker hooks caused by b09a0bf2b5f1e3a98d9561bd200d804d9c4b2ef2

--- lua/actionspooc.lua
+++ lua/actionspooc.lua
@@ -1,4 +1,4 @@
 -- Fix endless Cloaker beatdown
 function ActionSpooc:complete()
-	return self._beating_end_t and self._beating_end_t < TimerManager:game():time() and (not self:is_flying_strike() or self._last_vel_z >= 0)
+	return self._beating_end_t and self._beating_end_t < TimerManager:game():time() and (not self:is_flying_strike() or self._last_vel_z == 0)
 end
