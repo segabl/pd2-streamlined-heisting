181719e53a6d64703148d7892acd79bfcf6aebe4
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-06-07 14:56:06 +0200

    Decrease flashbang timer scaling
    Now only scales down to 1s on DS instead of 0.75s

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -470,7 +470,7 @@ Hooks:PostHook(GroupAITweakData, "_init_task_data", "sh__init_task_data", functi
 	self.smoke_grenade_timeout = { 25, 35 }
 	self.smoke_grenade_lifetime = math.lerp(9, 15, f)
 	self.flash_grenade_timeout = { 10, 15 }
-	self.flash_grenade.timer = math.lerp(2, 0.75, f)
+	self.flash_grenade.timer = math.lerp(2, 1, f)
 	self.cs_grenade_timeout = { 60, 90 }
 	self.cs_grenade_lifetime = math.lerp(20, 40, f)
 
