e9698a11f5c04ffa792b3a24b37731672d3166ca
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-06-14 12:42:23 +0200

    Slightly increase and scale point of no return timer on Hell's Island

---
+++ req/mission_script/bph.lua
@@ -0,0 +1,14 @@
+return {
+	-- Scale PonR with difficulty
+	[101161] = {
+		values = {
+			time_normal = 420,
+			time_hard = 405,
+			time_overkill = 390,
+			time_overkill_145 = 375,
+			time_easy_wish = 360,
+			time_overkill_290 = 345,
+			time_sm_wish = 330
+		}
+	}
+}
