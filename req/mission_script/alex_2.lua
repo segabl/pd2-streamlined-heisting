9b3eb4ecc2a0d77a4d4afded8779a0f76f6f3d85
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-08-21 21:03:52 +0200

    Fix stuck spawn groups on day 2 of Rats
    Prevents Shield and Bulldozer groups spawning from certain spawn points and getting stuck

---
+++ req/mission_script/alex_2.lua
@@ -0,0 +1,17 @@
+return {
+	[104480] = {
+		groups = {
+			tac_bull_rush = false,
+			tac_shield_wall = false,
+			tac_shield_wall_ranged = false,
+			tac_shield_wall_charge = false
+		}
+	},
+	[102793] = {
+		groups = {
+			tac_shield_wall = false,
+			tac_shield_wall_ranged = false,
+			tac_shield_wall_charge = false
+		}
+	}
+}
