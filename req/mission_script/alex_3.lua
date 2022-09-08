84e1df2f33bea92a0b537fbb85758787a5fdd006
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-09-08 22:49:36 +0200

    Fix gangsters and police shooting each other on Rats day 3

---
+++ req/mission_script/alex_3.lua
@@ -0,0 +1,113 @@
+return {
+	-- Set gangsters to law team (they are not supposed to shoot the cops)
+	[100506] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100507] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100788] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100789] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100790] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100791] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100793] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100794] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100796] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[100797] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101124] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101130] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101132] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101138] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101140] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101178] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101179] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101180] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101181] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101182] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101183] = {
+		values = {
+			team = "law1"
+		}
+	},
+	[101184] = {
+		values = {
+			team = "law1"
+		}
+	}
+}
