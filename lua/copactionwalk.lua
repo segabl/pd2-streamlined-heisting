6a0f98bae35d42c752f83a88cede54f09179e6f1
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-09-26 20:19:40 +0200

    Fix running animations sometimes playing too fast
    When turning and then running with guns up, enemies sometimes played their running animation too fast, fix courtesy of RedFlame

--- lua/copactionwalk.lua
+++ lua/copactionwalk.lua
@@ -14,3 +14,35 @@ end
 function CopActionWalk:get_destination_pos()
 	return self._nav_point_pos(self._simplified_path and self._simplified_path[#self._simplified_path] or self._nav_path and self._nav_path[#self._nav_path])
 end
+
+
+-- Fix occasional incorrect animation speed
+local idstr_base = Idstring("base")
+function CopActionWalk:_adjust_move_anim(side, speed)
+	local anim_data = self._ext_anim
+	if anim_data.haste == speed and anim_data["move_" .. side] then
+		return
+	end
+
+	local enter_t
+	local redirect_name = speed .. "_" .. side
+	local move_side = anim_data.move_side
+	if move_side and (side == move_side or self._matching_walk_anims[side][move_side]) then
+		local seg_rel_t = self._machine:segment_relative_time(idstr_base)
+
+		if not self._walk_anim_lengths[anim_data.pose] or not self._walk_anim_lengths[anim_data.pose][self._stance.name] or not self._walk_anim_lengths[anim_data.pose][self._stance.name][speed] or not self._walk_anim_lengths[anim_data.pose][self._stance.name][speed][side] then
+			return
+		end
+
+		local walk_anim_length = self._walk_anim_lengths[anim_data.pose][self._stance.name][speed][side]
+		enter_t = seg_rel_t * walk_anim_length
+	end
+
+	local could_freeze = anim_data.can_freeze and anim_data.upper_body_empty
+	local redir_res = self._ext_movement:play_redirect(redirect_name, enter_t)
+	if could_freeze then
+		self._ext_base:chk_freeze_anims()
+	end
+
+	return redir_res
+end
