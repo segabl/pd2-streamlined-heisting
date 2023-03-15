857da75253c6870dd5564395c6694a78edd48a62
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-03-15 03:20:25 +0100

    Rework Cloaker standing up code
    Reworked the code that forced Cloakers to stand up to be more reliable and catch more edge cases

--- lua/spooclogicattack.lua
+++ lua/spooclogicattack.lua
@@ -1,5 +1,4 @@
 -- More consistent Cloaker attacks, why wouldn't they be allowed to start a sprint attack when another cop is in the way?
--- Also force Cloakers to stand up before starting a charge
 function SpoocLogicAttack._upd_spooc_attack(data, my_data)
 	if my_data.spooc_attack or data.t <= data.spooc_attack_timeout_t or data.unit:movement():chk_action_forbidden("walk") then
 		return
@@ -22,10 +21,10 @@ function SpoocLogicAttack._upd_spooc_attack(data, my_data)
 		return
 	end
 
-	if not data.spooc_attack_delay_t then
-		data.spooc_attack_delay_t = focus_enemy.verified_t + 0.35
+	if not my_data.spooc_attack_delay_t then
+		my_data.spooc_attack_delay_t = focus_enemy.verified_t + 0.35
 		return
-	elseif data.spooc_attack_delay_t > data.t then
+	elseif my_data.spooc_attack_delay_t > data.t then
 		return
 	end
 
@@ -41,16 +40,9 @@ function SpoocLogicAttack._upd_spooc_attack(data, my_data)
 		my_data.attention_unit = focus_enemy.u_key
 	end
 
-	if not flying_strike then
-		data.unit:movement():action_request({
-			body_part = 4,
-			type = "stand"
-		})
-	end
-
 	local action = SpoocLogicAttack._chk_request_action_spooc_attack(data, my_data, flying_strike)
 	if action then
-		data.spooc_attack_delay_t = nil
+		my_data.spooc_attack_delay_t = nil
 
 		my_data.spooc_attack = {
 			start_t = data.t,
--- lua/unitnetworkhandler.lua
+++ lua/unitnetworkhandler.lua
@@ -1,21 +1,3 @@
--- Force Cloakers to stand up before starting an attack on clients
-Hooks:PreHook(UnitNetworkHandler, "action_spooc_start", "sh_action_spooc_start", function (self, unit)
-	if not self._verify_character(unit) or not self._verify_gamestate(self._gamestate_filter.any_ingame) then
-		return
-	end
-
-	unit:movement():action_request({
-		body_part = 4,
-		type = "stand"
-	})
-
-	unit:movement():action_request({
-		body_part = 3,
-		type = "idle"
-	})
-end)
-
-
 -- Properly sync reload
 function UnitNetworkHandler:reload_weapon_cop(cop, sender)
 	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character_and_sender(cop, sender) then
