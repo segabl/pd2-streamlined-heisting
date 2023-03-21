1edfc428fda7a7db867935eaad5359f04fe4d9f5
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-03-21 19:58:50 +0100

    Fix issue where travel logic code was called outside of travel logic

--- lua/coplogictravel.lua
+++ lua/coplogictravel.lua
@@ -22,19 +22,24 @@ CopLogicTravel.on_area_safety = CopLogicIdle.on_area_safety
 Hooks:PostHook(CopLogicTravel, "enter", "sh_enter", CopLogicTravel.upd_advance)
 
 function CopLogicTravel.on_pathing_results(data)
+	local my_data = data.internal_data
+
+	CopLogicTravel._upd_pathing(data, my_data)
+
+	if data.internal_data ~= my_data then
+		return
+	end
+
 	CopLogicTravel.upd_advance(data)
 end
 
 
--- Fix need for another queued task to update pathing or leaving cover on expired cover time
--- Basically just does the needed checks before calling the original function to save on a queued update
+-- Fix need for another queued task to update pathing after expired cover leave time
 Hooks:PreHook(CopLogicTravel, "upd_advance", "sh_upd_advance", function (data)
 	local unit = data.unit
 	local my_data = data.internal_data
 	local t = TimerManager:game():time()
-	if my_data.processing_advance_path or my_data.processing_coarse_path then
-		CopLogicTravel._upd_pathing(data, my_data)
-	elseif my_data.cover_leave_t then
+	if my_data.cover_leave_t then
 		if my_data.coarse_path and my_data.coarse_path_index == #my_data.coarse_path or my_data.cover_leave_t < t and not unit:movement():chk_action_forbidden("walk") and not data.unit:anim_data().reload then
 			my_data.cover_leave_t = nil
 		end
