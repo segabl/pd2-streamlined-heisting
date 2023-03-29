914826ebb6e6a1f8e9ecdd5fc8ebe9b64c8092c4
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-03-29 21:34:32 +0200

    Enable flashlights on some night time heists
    This makes enemies use flashlights on those heists if the "More NPC Flashlights" mod is used

---
+++ lua/levelstweakdata.lua
@@ -0,0 +1,12 @@
+-- Enable flashlight state on some night time levels
+Hooks:PostHook(LevelsTweakData, "init", "sh_init", function (self)
+	self.arm_fac.flashlights_on = true
+	self.arm_und.flashlights_on = true
+	self.dah.flashlights_on = true
+	self.escape_park.flashlights_on = true
+	self.firestarter_1.flashlights_on = true
+	self.firestarter_2.flashlights_on = true
+	self.watchdogs_1_night.flashlights_on = true
+	self.watchdogs_2.flashlights_on = true
+	self.welcome_to_the_jungle_1_night.flashlights_on = true
+end)
