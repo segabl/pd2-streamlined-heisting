16bc57db55cc9bb1235765a56c6f6e2b48594bac
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-04-04 23:20:57 +0200

    Fix crash when putting bags on certain ziplines

--- lua/carrydata.lua
+++ lua/carrydata.lua
@@ -1,3 +1,7 @@
+if not Network:is_server() then
+	return
+end
+
 -- Tweak bag stealing conditions
 function CarryData:clbk_pickup_SO_verification(unit)
 	if not self._steal_SO_data or not self._steal_SO_data.SO_id then
@@ -31,3 +35,29 @@ function CarryData:clbk_pickup_SO_verification(unit)
 		return true
 	end
 end
+
+
+-- Fix zipline trying to copy non existing bag attention data
+Hooks:OverrideFunction(CarryData, "set_zipline_unit", function (self, zipline_unit)
+	self._zipline_unit = zipline_unit
+	self:_set_expire_enabled(not self._zipline_unit)
+
+	if managers.groupai:state():enemy_weapons_hot() then
+		return
+	end
+
+	if self._zipline_unit and self._zipline_unit:zipline():ai_ignores_bag() then
+		local attention_data = self._unit:attention() and self._unit:attention():attention_data()
+		if attention_data then
+			self._saved_attention_data = deep_clone(attention_data)
+			for attention_id, _ in pairs(self._saved_attention_data) do
+				self._unit:attention():remove_attention(attention_id)
+			end
+		end
+	elseif not self._zipline_unit and self._saved_attention_data then
+		for _, attention_data in pairs(self._saved_attention_data) do
+			self._unit:attention():add_attention(attention_data)
+		end
+		self._saved_attention_data = nil
+	end
+end)
