f3e2ea926b99e4aa7f7d9a165ba4b20821ea0f55
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-05-02 19:44:20 +0200

    Ignore status of player criminals for balancing multiplier

--- lua/groupaistatebase.lua
+++ lua/groupaistatebase.lua
@@ -250,7 +250,7 @@ Hooks:PreHook(GroupAIStateBase, "add_special_objective", "sh_add_special_objecti
 end)
 
 
--- Fully count bots for balancing multiplier
+-- Fully count all criminals for the balancing multiplier
 function GroupAIStateBase:_get_balancing_multiplier(balance_multipliers)
-	return balance_multipliers[math.clamp(table.count(self:all_char_criminals(), function (u_data) return not u_data.status end), 1, #balance_multipliers)]
+	return balance_multipliers[math.clamp(table.size(self._char_criminals), 1, #balance_multipliers)]
 end
