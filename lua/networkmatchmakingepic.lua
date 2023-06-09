2ce6e618decf68681ca248eb4a1caa274594d6f7
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-09 11:42:24 +0200

    Add matchmaking key for Epic

---
+++ lua/networkmatchmakingepic.lua
@@ -0,0 +1,1 @@
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
--- lua/networkmatchmakingsteam.lua
+++ lua/networkmatchmakingsteam.lua
@@ -1,3 +1,1 @@
--- Set matchmaking key by appending mod info (appending will keep vanilla versioning intact)
 NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
-StreamHeist:log("Matchmaking key set to", NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY)
