6d556beb1ba1a7539f257ca55d8995f42e83b129
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-10 10:57:18 +0200

    Move matchmaking keys to network manager

--- lua/networkmanager.lua
+++ lua/networkmanager.lua
@@ -1,1 +1,2 @@
 NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
--- lua/networkmatchmakingepic.lua
+++
@@ -1,1 +0,0 @@
-NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_sh_v" .. StreamHeist.mod_instance:GetVersion()
