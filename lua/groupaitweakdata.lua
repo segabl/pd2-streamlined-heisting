b315df0faa8cbf1dab0eefd8142b9fa298a7cb8e
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-07-16 16:54:47 +0200

    Tweak some enemy chatter sound queue durations

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -92,12 +92,13 @@ end)
 -- Fix retreat chatter using the wrong voiceline and make voiclines used for pushing unique
 Hooks:PostHook(GroupAITweakData, "_init_chatter_data", "sh__init_chatter_data", function (self)
 	self.enemy_chatter.retreat.queue = "m01"
+	self.enemy_chatter.retreat.duration = { 4, 8 }
 	self.enemy_chatter.push = {
 		radius = 1000,
 		max_nr = 0,
 		queue = "pus",
 		group_min = 0,
-		duration = { 10, 10 },
+		duration = { 5, 10 },
 		interval = { 0.75, 1.2 }
 	}
 end)
