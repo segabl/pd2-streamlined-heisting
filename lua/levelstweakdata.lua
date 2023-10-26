7b6b3ed28ea2e21aee66caa93328d1b3de9d8e48
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-10-26 22:17:59 +0200

    Make enemies use flashlights on Nightclub and Election Day 2

--- lua/levelstweakdata.lua
+++ lua/levelstweakdata.lua
@@ -3,9 +3,11 @@ Hooks:PostHook(LevelsTweakData, "init", "sh_init", function (self)
 	self.arm_fac.flashlights_on = true
 	self.arm_und.flashlights_on = true
 	self.dah.flashlights_on = true
+	self.election_day_2.flashlights_on = true
 	self.escape_park.flashlights_on = true
 	self.firestarter_1.flashlights_on = true
 	self.firestarter_2.flashlights_on = true
+	self.nightclub.flashlights_on = true
 	self.watchdogs_1_night.flashlights_on = true
 	self.watchdogs_2.flashlights_on = true
 	self.welcome_to_the_jungle_1_night.flashlights_on = true
