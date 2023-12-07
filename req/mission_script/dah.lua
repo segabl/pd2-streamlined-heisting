65e43cac42634471be46fcf165e7fa7268d7fa33
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-12-07 18:15:25 +0100

    Improve mission scripting
    Increased delay on vault spawngroups
    Reduced initial group spawn delay on Bank Heist

---
+++ req/mission_script/dah.lua
@@ -0,0 +1,23 @@
+return {
+	-- Slow down vault group spawns
+	[100722] = {
+		values = {
+			interval = 30
+		}
+	},
+	[100723] = {
+		values = {
+			interval = 30
+		}
+	},
+	[104821] = {
+		values = {
+			interval = 30
+		}
+	},
+	[104822] = {
+		values = {
+			interval = 30
+		}
+	}
+}
