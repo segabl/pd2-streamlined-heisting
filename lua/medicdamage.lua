113cf59f589b30c27cc714af74d467464c9c8845
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-03-14 10:08:56 +0100

    Fix medics healing during full body actions

--- lua/medicdamage.lua
+++ lua/medicdamage.lua
@@ -9,3 +9,13 @@ function MedicDamage:verify_heal_requesting_unit(requesting_unit, ...)
 	local medic_pos = self._unit:movement():m_head_pos()
 	return not World:raycast("ray", unit_pos, medic_pos, "slot_mask", managers.slot:get_mask("AI_visibility"), "report")
 end
+
+
+-- Fix medics healing during full body actions
+local is_available_for_healing_original = MedicDamage.is_available_for_healing
+function MedicDamage:is_available_for_healing(requesting_unit, ...)
+	if self._unit:movement():chk_action_forbidden("act") then
+		return false
+	end
+	return is_available_for_healing_original(self, requesting_unit, ...)
+end
