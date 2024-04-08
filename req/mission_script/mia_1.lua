ab58617567c203b9d35635e8c3538ecfd68ac374
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-04-08 21:08:18 +0200

    Improve Hotline Miami scripting and navigation
    Moved reinforce spots on day 1
    Fixed unused scaffolding spawn location on day 2
    Slightly changed roof navigation to distribute enemies better on day 2

---
+++ req/mission_script/mia_1.lua
@@ -0,0 +1,22 @@
+return {
+	-- Improve reinforce spots
+	[100022] = {
+		reinforce = {
+			{
+				name = "touch_grass",
+				force = 3,
+				position = Vector3(2000, -900, 30)
+			}
+		}
+	},
+	[100589] = {
+		values = {
+			enabled = false
+		}
+	},
+	[100590] = {
+		values = {
+			enabled = false
+		}
+	}
+}
