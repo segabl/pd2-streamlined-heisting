a5a8272ce8e7b145bf26cf027e4f4b589b1d46ba
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-01-27 21:33:54 +0100

    Fix enemies shooting while hurt using incorrect directions

--- lua/copactionhurt.lua
+++ lua/copactionhurt.lua
@@ -109,3 +109,18 @@ function CopActionHurt:_pseudorandom(a, b)
 		return val
 	end
 end
+
+
+-- Fix shooting direction while hurt being specified as rotation instead of a vector
+function CopActionHurt:clbk_shooting_hurt()
+	self._delayed_shooting_hurt_clbk_id = nil
+
+	if not alive(self._weapon_unit) then
+		return
+	end
+
+	local fire_obj = self._weapon_unit:base().fire_object and self._weapon_unit:base():fire_object()
+	if fire_obj then
+		self._weapon_unit:base():singleshot(fire_obj:position(), fire_obj:rotation():y())
+	end
+end
