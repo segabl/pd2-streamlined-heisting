ff67ab4d8c1feb7991c4dfb3712cde52cdc87962
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-01-27 21:35:15 +0100

    Add unit level overrides for Reservoir Dogs and Dragon Heist
    These heists will now spawn their respective police units when spawned as part of enemy groups

--- lua/levelstweakdata.lua
+++ lua/levelstweakdata.lua
@@ -1,5 +1,6 @@
 -- Enable flashlight state on some night time levels
-Hooks:PostHook(LevelsTweakData, "init", "sh_init", function (self)
+-- Add level specific unit overrides
+Hooks:PostHook(LevelsTweakData, "init", "sh_init", function(self)
 	self.arm_fac.flashlights_on = true
 	self.arm_und.flashlights_on = true
 	self.dah.flashlights_on = true
@@ -12,4 +13,34 @@ Hooks:PostHook(LevelsTweakData, "init", "sh_init", function (self)
 	self.watchdogs_1_night.flashlights_on = true
 	self.watchdogs_2.flashlights_on = true
 	self.welcome_to_the_jungle_1_night.flashlights_on = true
+
+	self.chas.ai_unit_group_overrides = {
+		CS_cop_C45_R870 = {
+			america = {
+				Idstring("units/pd2_dlc_chas/characters/ene_male_chas_police_02/ene_male_chas_police_02")
+			}
+		},
+		CS_cop_stealth_MP5 = {
+			america = {
+				Idstring("units/pd2_dlc_chas/characters/ene_male_chas_police_01/ene_male_chas_police_01")
+			}
+		}
+	}
+	self.sand.ai_unit_group_overrides = self.chas.ai_unit_group_overrides
+
+	self.rvd1.ai_unit_group_overrides = {
+		CS_cop_C45_R870 = {
+			america = {
+				Idstring("units/pd2_dlc_rvd/characters/ene_la_cop_1/ene_la_cop_1"),
+				Idstring("units/pd2_dlc_rvd/characters/ene_la_cop_3/ene_la_cop_3"),
+				Idstring("units/pd2_dlc_rvd/characters/ene_la_cop_4/ene_la_cop_4")
+			}
+		},
+		CS_cop_stealth_MP5 = {
+			america = {
+				Idstring("units/pd2_dlc_rvd/characters/ene_la_cop_2/ene_la_cop_2")
+			}
+		}
+	}
+	self.rvd2.ai_unit_group_overrides = self.rvd1.ai_unit_group_overrides
 end)
