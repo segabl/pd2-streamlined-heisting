0122a6942962d6e6dbe96245efc441ba40f7b764
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-03-10 23:05:32 +0100

    Fix weapon swaps not hiding the original weapon
    In some cases when enemies had their weapons swapped their original weapon wouldn't be hidden correctly

---
+++ lua/playerinventory.lua
@@ -0,0 +1,16 @@
+-- Fix weapon swaps not disabling the original weapon correctly if it has a different selection index
+local _place_selection_original = PlayerInventory._place_selection
+function PlayerInventory:_place_selection(selection_index, is_equip, ...)
+	local next_update_funcs_index = #_next_update_funcs
+
+	_place_selection_original(self, selection_index, is_equip, ...)
+
+	if is_equip and #_next_update_funcs > next_update_funcs_index then
+		local update_func = _next_update_funcs[next_update_funcs_index + 1]
+		_next_update_funcs[#_next_update_funcs] = function()
+			if self._equipped_selection == selection_index then
+				update_func()
+			end
+		end
+	end
+end
