d4fca794db6bea02a1d68825c94853affe545363
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-09-04 18:05:11 +0200

    Fix Shield ambush on Watchdogs day 1
    Shields will no longer immediately back up or swarm the truck

--- req/mission_script/watchdogs_1.lua
+++ req/mission_script/watchdogs_1.lua
@@ -1,4 +1,31 @@
+local shield_so = {
+	pre_func = function(element)
+		if Network:is_client() then
+			return
+		end
+		element:add_event_callback("spawn", function(unit)
+			local pos = unit:movement():m_pos()
+			unit:brain():set_objective({
+				type = "sniper",
+				pos = pos,
+				nav_seg = managers.navigation:get_nav_seg_from_pos(pos),
+				no_retreat = true
+			})
+		end)
+	end
+}
 return {
+	-- Combine some navigation areas
+	[100125] = {
+		ai_area = {
+			{ 42, 75 },
+			{ 51, 76 },
+			{ 52, 134 },
+			{ 81, 166, 167 },
+			{ 127, 129 }
+		}
+	},
+	-- Fix group spawns
 	[101687] = {
 		groups = {
 			tac_bull_rush = false,
@@ -17,5 +44,10 @@ return {
 		on_executed = {
 			{ id = 100768, delay = 30 }
 		}
-	}
+	},
+	-- Set shields to stay in place
+	[102848] = shield_so,
+	[102849] = shield_so,
+	[102850] = shield_so,
+	[102851] = shield_so
 }
